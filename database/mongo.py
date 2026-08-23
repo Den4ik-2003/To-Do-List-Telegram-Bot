e278db94d714b7a54a914cc0c899cc5a444baacd
Denys Romanyshyn <DenysYkr135@gmail.com>
2026-08-23 14:29:21 +0300

    Update

--- database/mongo.py
+++ database/mongo.py
@@ -20,12 +20,14 @@ transactions_col = None
 budgets_col = None
 ai_usage_col = None
 ai_conversations_col = None
+olx_tracked_col = None
 
 
 async def init_mongo(mongo_uri: str):
     global mongo_client, db, tasks_col, users_col, auth_col, counters_col
     global goals_col, projects_col, rates_col, events_col
     global transactions_col, budgets_col, ai_usage_col, ai_conversations_col
+    global olx_tracked_col
 
     mongo_client = AsyncIOMotorClient(
         mongo_uri,
@@ -49,6 +51,7 @@ async def init_mongo(mongo_uri: str):
     budgets_col = db["budgets"]
     ai_usage_col = db["ai_usage"]
     ai_conversations_col = db["ai_conversations"]
+    olx_tracked_col = db["olx_tracked"]
 
     await ping()
     return db
